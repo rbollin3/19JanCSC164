e9c21264e894bcd56ff5f4f9e3f7920c901db12a
unknown <rbollin3@W-261-19.dtcc.edu>
2016-01-19 15:36:26 -0500

    Initial commit

---
+++ src/P1/Main.java
@@ -0,0 +1,21 @@
+package P1;
+
+import java.time.Month;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+        double [] MonthlySales = {1590.75,7890.15,6090.15};
+
+
+
+
+        for(double each: MonthlySales)
+        {
+            System.out.println(each);
+        }
+    }
+}
